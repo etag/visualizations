99f9688653dbe526e7ea13be456d302904dd2cef
nbgraham <nickbgraham83@gmail.com>
2015-10-21 21:43:33 -0500

    Tried to use data from API
    Downloaded API JSON files to avoid cross origin errors
    Cannot figure out the asynchronous d3.json calls

--- readermap/script.js
+++ readermap/script.js
@@ -17,128 +17,155 @@ var path = d3.geo.path()
 d3.json("/readermap/GeoJSON/usa.json", function(error, usa) {
   if(error) throw error;
 
-  d3.json("/sample_data.json", function(error, reads) {
+  d3.json("/tag_reads.json", function(error, reads) {
     if (error) throw error;
 
     try
     {
-      locations = type(reads.results);
-      var b = path.bounds(locations);
-      var s = .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
-      var t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+      locations = convertToGeoJSON(reads.results);
 
-      projection
-        .scale(s)
-        .translate(t);
+      if (locations.features.length > 0)
+      {
+        var b = path.bounds(locations);
+        var s = 0.95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
+        var t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
 
-      states = topojson.feature(usa, usa.objects.sts).features;
-      rivers = topojson.feature(usa, usa.objects.rivers).features;
-      lakes = topojson.feature(usa, usa.objects.lakes).features;
+        projection
+          .scale(s)
+          .translate(t);
 
-      console.log(states);
+        states = topojson.feature(usa, usa.objects.sts).features;
+        rivers = topojson.feature(usa, usa.objects.rivers).features;
+        lakes = topojson.feature(usa, usa.objects.lakes).features;
 
-      colorScale.domain([0,states.length]);
+        console.log(states);
 
-      svg.selectAll(".state")
-          .data(states)
-          .enter().append("path")
-          .attr("class", "state")
-          .attr("id", function(d) { return d.properties.iso_3166_2;})
-          .attr("d", path)
-          .attr("fill", function(d,i) {return colorScale(((i*i)/3+7)%50);});
+        colorScale.domain([0,states.length]);
 
-      svg.selectAll(".river")
-          .data(rivers)
-          .enter().append("path")
-          .attr("class", "river")
-          .attr("id", function(d) { return d.properties.name;})
-          .attr("d", path);
+        svg.selectAll(".state")
+            .data(states)
+            .enter().append("path")
+            .attr("class", "state")
+            .attr("id", function(d) { return d.properties.iso_3166_2;})
+            .attr("d", path)
+            .attr("fill", function(d,i) {return colorScale(((i*i)/3+7)%50);});
 
-      svg.selectAll(".lake")
-          .data(lakes)
-          .enter().append("path")
-          .attr("class", "lake")
-          .attr("id", function(d) { return d.properties.name;})
-          .attr("d", path);
+        svg.selectAll(".river")
+            .data(rivers)
+            .enter().append("path")
+            .attr("class", "river")
+            .attr("id", function(d) { return d.properties.name;})
+            .attr("d", path);
 
-      svg.selectAll(".reader-location")
-        .data(locations.features)
-        .enter().append("circle")
-        .attr("class", "reader-location")
-        .attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
-        .attr("r", radius)
-        .append("svg:title")
-        .text(function (d) {
-          var result = "Reader: " + d.properties.name + "\n";
-          for (var i = 0; i < d.properties.reads.length; i++)
+        svg.selectAll(".lake")
+            .data(lakes)
+            .enter().append("path")
+            .attr("class", "lake")
+            .attr("id", function(d) { return d.properties.name;})
+            .attr("d", path);
+
+        svg.selectAll(".reader-location")
+          .data(locations.features)
+          .enter().append("circle")
+          .attr("class", "reader-location")
+          .attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
+          .attr("r", radius)
+          .append("svg:title")
+          .text(function (d) {
+            var result = "Reader: " + d.properties.name + "\n";
+            for (var i = 0; i < d.properties.reads.length; i++)
+            {
+              result += "Tag ID: " + d.properties.reads[i].tag_id + " Time: " + new Date(d.properties.reads[i].timestamp) + "\n";
+            }
+            return result;
+          });
+        }
+        else {
           {
-            result += "Tag ID: " + d.properties.reads[i].tag_id + " Time: " + new Date(d.properties.reads[i].timestamp) + "\n";
+            console.log("0 locations");
           }
-          return result;
-        });
-    }
-    catch(err)
-    {
-      console.log(err);
-    }
+        }
+      }
+      catch(err)
+      {
+        console.log(err);
+      }
   });
 });
 
-function type(data)
-{
-  var result = {
-    "type": "FeatureCollection",
-    "features": []
+function convertToGeoJSON(data) {
+  var Result = function() {
+    this.type = "FeatureCollection";
+    this.features = [];
   };
 
-  for (var i = 0; i<data.length; i++)
-  {
-    var element = data[i];
-    var index = contains(result.features, element.reader);
-    var reader;
-    if (index < 0)
-    {
-      reader = {
-        "type": "Feature",
-        "geometry": swap(element.location),
-        "properties": {
-          "name": element.reader,
-          "reads" : []
+  Result.prototype.addReaderAndRead = function(element) {
+      var readers = this.features;
+      d3.json("/reader=" + element.reader + ".json", function(error, readerInfo) {
+        if (error) throw error;
+
+        var index = -1;
+
+        for (var i = 0; i < readers.length; i++)
+        {
+          if (readers[i].properties.name === element.reader)
+            index = i;
+            break;
         }
-      }
-      result.features.push(reader);
-    }
-    else
-    {
-      reader = result.features[index];
-      var elmCoord = swap(element.location).coordinates;
-      var rdrCoord = reader.geometry.coordinates;
-      if (elmCoord[0] !== rdrCoord[0] || elmCoord[1] != rdrCoord[1]) throw "Reader " + reader.properties.name + " has two different locations.";
-    }
 
-    var read = {
-      "timestamp": element.timestamp,
-      "tag_id": element.tag_id
-    };
+        if (index < 0)
+        {
+          var readerCorrect = searchTimestamps(readerInfo.results, element.tag_timestamp);
+          reader = {
+            "type": "Feature",
+            "geometry": {
+              type: "Point",
+              coordinates: [readerCorrect.longitude, readerCorrect.latitude]
+            },
+            "properties": {
+              "name": element.reader,
+              "reads" : []
+            }
+          };
+          readers.push(reader);
+        }
+        else
+        {
+          reader = readers[index];
+        }
 
-    reader.properties.reads.push(read);
+        var read = {
+          "timestamp": element.tag_timestamp,
+          "tag_id": element.tag
+        };
+
+        reader.properties.reads.push(read);
+      });
   }
 
+  var result = new Result();
+  for (var i = 0; i < data.length; i++)
+  {
+    result.addReaderAndRead(data[i]);
+  }
   return result;
 }
 
-function contains(list, value)
-{
-  for (var i = 0; i < list.length; i++)
+function searchTimestamps(readers, timestamp) {
+  timestamp = new Date(timestamp);
+  for (var i = 0; i < readers.length; i++)
   {
-    if (list[i].properties.name === value)
-      return i;
+    if (new Date(readers[i].start_timestamp) < timestamp &&
+      (readers[i].end_timestamp == null || timestamp < new Date(readers[i].end_timestamp)))
+    {
+      return readers[i];
+    }
   }
   return -1;
 }
 
-function swap(location)
-{
+
+function swap(location) {
   var temp = location.coordinates[0];
   location.coordinates[0] = location.coordinates[1];
   location.coordinates[1] = temp;
