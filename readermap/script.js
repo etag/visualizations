a2f608e083982166296baf4aae46d9fe1e3d335d
nbgraham <nickbgraham83@gmail.com>
2015-10-22 20:30:36 -0500

    Added combo box to load and show data

--- readermap/script.js
+++ readermap/script.js
@@ -3,6 +3,7 @@ var radius = 5;
 var width = 600;
 
 //Set up
+
 var height = width * 500 / 960;
 var svg = d3.select("body").append("svg")
     .attr("width", width)
@@ -15,127 +16,159 @@ var path = d3.geo.path()
   .projection(projection)
   .pointRadius(radius);
 
+var coordinates = d3.select("body").append("div");
+var combo = d3.select("body").append("select").attr("id","combo");
+
+d3.json("http://head.ouetag.org/api/etag/tags/.json", function(error, tags) {
+  tags.results.forEach(function(tag) {
+    combo.append("option").attr("value",tag.url).text(tag.tag_id);
+  });
+});
+
 //Read in states, rivers, and lakes for map
 d3.json("/readermap/GeoJSON/usa.json", function(error, usa) {
   if(error) throw error;
 
-  //Read in tag reads
-  d3.json("/tag_reads.json", function(error, reads) {
-    if (error) throw error;
+  //Setup
+  states = topojson.feature(usa, usa.objects.sts).features;
+  rivers = topojson.feature(usa, usa.objects.rivers).features;
+  lakes = topojson.feature(usa, usa.objects.lakes).features;
+  colorScale.domain([0,states.length]);
 
-    try
-    {
-      var locations = {
-        "type": "FeatureCollection",
-        "features": []
-      };
-      var element;
-      var elementTime;
-      var index;
-      d3.json("http://head.ouetag.org/api/etag/reader_location/.json", function(error, readersInfo) {
-        for (var i = 0; i < reads.results.length; i++)
-        {
-            element = reads.results[i];
-            index = -1;
-            elementTime = new Date(element.tag_timestamp);
+  combo.on("change", function() {
+    //Read in tag reads
+    d3.json("http://head.ouetag.org/api/etag/tag_reads/.json?page_size=1000&tag="+this.options[this.selectedIndex].text, function(error, reads) {
+      if (error) throw error;
 
-            for (var j = 0; j < locations.features.length; j++)
-            {
-              if (locations.features[j].properties.name === element.reader && new Date(locations.features[j].start_timestamp) < elementTime &&
-                (locations.features[j].end_timestamp == null || elementTime < new Date(locations.features[j].end_timestamp)))
+        var locations = {
+          "type": "FeatureCollection",
+          "features": []
+        };
+        var element;
+        var elementTime;
+        var index;
+        d3.json("http://head.ouetag.org/api/etag/reader_location/.json", function(error, readersInfo) {
+
+          if (reads.results.length === 0) alert("Tag " + reads.tag + " has no reads!");
+          for (var i = 0; i < reads.results.length; i++)
+          {
+              element = reads.results[i];
+              index = -1;
+              elementTime = new Date(element.tag_timestamp);
+
+              for (var j = 0; j < locations.features.length; j++)
               {
-                index = j;
-                break;
-              }
-            }
+                var currReaderProperties = locations.features[j].properties;
 
-            if (index < 0)
-            {
-              var readerCorrect = searchForReader(readersInfo.results, element);
-              reader = {
-                "type": "Feature",
-                "geometry": {
-                  type: "Point",
-                  coordinates: [readerCorrect.longitude, readerCorrect.latitude]
-                },
-                "properties": {
-                  "name": element.reader,
-                  "reads" : []
+                if ( currReaderProperties.name === element.reader && new Date(currReaderProperties.start_timestamp) < elementTime
+                  && (currReaderProperties.end_timestamp == null || elementTime < new Date(currReaderProperties.end_timestamp)))
+                {
+                  index = j;
+                  break;
                 }
+              }
+
+              if (index < 0)
+              {
+                var readerCorrect = searchForReader(readersInfo.results, element);
+                reader = {
+                  "type": "Feature",
+                  "geometry": {
+                    "type": "Point",
+                    "coordinates": [readerCorrect.longitude, readerCorrect.latitude]
+                  },
+                  "properties": {
+                    "name": element.reader,
+                    "reads" : [],
+                    "start_timestamp": readerCorrect.start_timestamp,
+                    "end_timestamp": readerCorrect.end_timestamp
+                  }
+                };
+                coordinates.text(reader.geometry.coordinates);
+                locations.features.push(reader);
+              }
+              else
+              {
+                reader = locations.features[index];
+              }
+
+              var read = {
+                "timestamp": element.tag_timestamp,
+                "tag_id": element.tag
               };
-              locations.features.push(reader);
-            }
-            else
-            {
-              reader = locations.features[index];
-            }
 
-            var read = {
-              "timestamp": element.tag_timestamp,
-              "tag_id": element.tag
-            };
+              reader.properties.reads.push(read);
+          }
 
-            reader.properties.reads.push(read);
-        }
-
-      if (locations.features.length > 0)
-      {
-        //Setup
-        states = topojson.feature(usa, usa.objects.sts).features;
-        rivers = topojson.feature(usa, usa.objects.rivers).features;
-        lakes = topojson.feature(usa, usa.objects.lakes).features;
-        colorScale.domain([0,states.length]);
-
-        //Zoom in to the correct area of the map
-        var b = path.bounds(locations);
-
-        //Change bounds if there is only one points so that s does not equal infinity
-        if (locations.features.length === 1)
+        if (locations.features.length > 0)
         {
-          b[1][0] += 0.01;
-          b[1][1] += 0.01;
-        }
-        var s = 0.95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
-        var t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
-        projection
-          .scale(s)
-          .translate(t);
+          //Zoom in to the correct area of the map
+          projection
+            .scale(1)
+            .translate([0, 0]);
+          var b = path.bounds(locations);
 
-        console.log(rivers);
-        //Draw the states
-        svg.selectAll(".state")
-            .data(states)
-            .enter().append("path")
+          //Change bounds if there is only one points so that s does not equal infinity
+          if (b[0][0] === b[1][0] && b[0][1] === b[1][1])
+          {
+            b[0][0] *= 0.9;
+            b[0][1] *= 0.9;
+
+            b[1][0] *= 1.1;
+            b[1][1] *= 1.1;
+          }
+
+          var s = 0.95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
+          var t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+          projection
+            .scale(s)
+            .translate(t);
+
+          console.log(b,s,t);
+
+          //Bind data
+          var statesSvg = svg.selectAll(".state").data(states);
+          var riversSvg = svg.selectAll(".river").data(rivers);
+          var lakesSvg = svg.selectAll(".lake").data(lakes);
+          var readersSvg = svg.selectAll(".reader-location").data(locations.features);
+
+          //Update
+          statesSvg.classed("updated", true);
+          riversSvg.classed("updated", true);
+          lakesSvg.classed("updated", true);
+          readersSvg.classed("updated", true);
+
+          //Enter
+          statesSvg.enter()
+            .append("path")
             .attr("class", "state")
             .attr("id", function(d) {return d.id;})
-            .attr("d", path)
             .attr("fill", function(d,i) {return colorScale(((i*i)/3+7)%50);});
 
-        //Draw the rivers
-        svg.selectAll(".river")
-            .data(rivers)
-            .enter().append("path")
+          riversSvg.enter()
+            .append("path")
             .attr("class", "river")
-            .attr("id", function(d) { return d.properties.name;})
-            .attr("d", path);
+            .attr("id", function(d) { return d.properties.name;});
 
-        //Draw the lakes
-        svg.selectAll(".lake")
-            .data(lakes)
-            .enter().append("path")
+          lakesSvg.enter()
+            .append("path")
             .attr("class", "lake")
-            .attr("id", function(d) { return d.properties.name;})
-            .attr("d", path);
+            .attr("id", function(d) { return d.properties.name;});
 
-        //Draw the reader locations
-        svg.selectAll(".reader-location")
-          .data(locations.features)
-          .enter().append("circle")
-          .attr("class", "reader-location")
-          .attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
-          .attr("r", radius)
-          .append("svg:title")
-          .text(function (d) {
+          readersSvg.enter()
+            .append("circle")
+            .attr("r", radius)
+            .attr("class", "reader-location")
+            .append("svg:title");
+
+
+          //Enter + Update
+          statesSvg.attr("d", path);
+          riversSvg.attr("d", path);
+          lakesSvg.attr("d", path);
+          readersSvg.attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
+            .select("title")
+            .text(function (d) {
             var result = "Reader: " + d.properties.name + "\n";
             for (var i = 0; i < d.properties.reads.length; i++)
             {
@@ -143,35 +176,17 @@ d3.json("/readermap/GeoJSON/usa.json", function(error, usa) {
             }
             return result;
           });
-        }
-        else {
-            console.log("0 reader locations");
-        }
-      });
-    }
-    catch(err)
-    {
-      console.log(err);
-    }
+
+          //Exit
+          statesSvg.exit().remove();
+          riversSvg.exit().remove();
+          lakesSvg.exit().remove();
+          readersSvg.exit().remove();
+          }
+        });
+    });
   });
 });
-
-function convertToGeoJSON(data) {
-
-}
-
-function searchTimestamps(readers, timestamp) {
-  timestamp = new Date(timestamp);
-  for (var i = 0; i < readers.length; i++)
-  {
-    if (new Date(readers[i].start_timestamp) < timestamp &&
-      (readers[i].end_timestamp == null || timestamp < new Date(readers[i].end_timestamp)))
-    {
-      return readers[i];
-    }
-  }
-  return -1;
-}
 
 function searchForReader(readers,element)
 {
