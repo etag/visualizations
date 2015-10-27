fb2bb71b12331e81e7e4955c3dedb683ae9c787d
nbgraham <nickbgraham83@gmail.com>
2015-10-27 14:20:26 -0500

    Added zoom slider

--- readermap/script.js
+++ readermap/script.js
@@ -19,7 +19,14 @@ var path = d3.geo.path()
 var combo = d3.select("body").append("select").attr("id","combo");
 var readerInfoArea = d3.select("body").append("div");
 
+var slider = d3.select("body").append("input")
+  .attr("type", "range")
+  .attr("max", 100)
+  .attr("min", 0.1)
+  .attr("value", 10);
+
 d3.json("http://head.ouetag.org/api/etag/tags/.json", function(error, tags) {
+  combo.append("option").attr("value","Show all readers").text("Show all readers").attr("selected", true);
   tags.results.forEach(function(tag) {
     combo.append("option").attr("value",tag.url).text(tag.tag_id);
   });
@@ -37,39 +44,145 @@ d3.json("/readermap/GeoJSON/usa.json", function(error, usa) {
       lakes = topojson.feature(usa, usa.objects.lakes).features;
       colorScale.domain([0,states.length]);
 
-      drawReaders(readersInfo);
+      drawReaders(readersInfo, 10);
 
       combo.on("change", function() {
-        //Read in tag reads
-        d3.json("http://head.ouetag.org/api/etag/tag_reads/.json?page_size=" + page_size + "&tag="+this.options[this.selectedIndex].text, function(error, reads) {
-          if (error) throw error;
-          update(reads, readersInfo);
-        });
+        if (this.options[this.selectedIndex].text === "Show all readers")
+        {
+          drawReaders(readersInfo);
+        }
+        else {
+          //Read in tag reads
+          d3.json("http://head.ouetag.org/api/etag/tag_reads/.json?page_size=" + page_size + "&tag="+this.options[this.selectedIndex].text, function(error, reads) {
+            if (error) throw error;
+            update(reads, readersInfo);
+          });
+        }
+      });
+
+      slider.on("input", function(){
+        drawPoints(0);
       });
     });
   });
 
 function drawReaders(readersInfo)
 {
+
   var locations = {
     "type": "FeatureCollection",
     "features": []
   };
 
-  var readerPoint;
-
   readersInfo.results.forEach(function(reader) {
-    readerPoint = {
+    locations.features.push({
       "type": "Feature",
       "geometry": {
         "type": "Point",
         "coordinates": [reader.longitude, reader.latitude]
       },
       "properties": reader
-    }
-      locations.features.push(readerPoint);
+    });
   });
 
+  drawPoints(locations);
+}
+
+function update(reads, readersInfo)
+{
+  var locations = {
+    "type": "FeatureCollection",
+    "features": []
+  };
+
+  var element;
+  var elementTime;
+  var index;
+
+    if (reads.results.length === 0) alert("Tag " + reads.tag + " has no reads!");
+    for (var i = 0; i < reads.results.length; i++)
+    {
+        element = reads.results[i];
+        index = -1;
+        elementTime = new Date(element.tag_timestamp);
+
+        for (var j = 0; j < locations.features.length; j++)
+        {
+          var currReaderProperties = locations.features[j].properties;
+
+          if ( currReaderProperties.name === element.reader && new Date(currReaderProperties.start_timestamp) < elementTime
+            && (currReaderProperties.end_timestamp == null || elementTime < new Date(currReaderProperties.end_timestamp)))
+          {
+            index = j;
+            break;
+          }
+        }
+
+        if (index < 0)
+        {
+          var readerCorrect = searchForReader(readersInfo.results, element);
+          reader = {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [readerCorrect.longitude, readerCorrect.latitude]
+            },
+            "properties": {
+              "name": element.reader,
+              "reads" : [],
+              "start_timestamp": readerCorrect.start_timestamp,
+              "end_timestamp": readerCorrect.end_timestamp
+            }
+          };
+          locations.features.push(reader);
+        }
+        else
+        {
+          reader = locations.features[index];
+        }
+
+        var read = {
+          "timestamp": element.tag_timestamp,
+          "tag_id": element.tag
+        };
+
+        reader.properties.reads.push(read);
+    }
+
+    drawPoints(locations);
+}
+
+function searchForReader(readers,element)
+{
+  var elementTime = new Date(element.tag_timestamp);
+  for (var i = 0; i < readers.length; i++)
+  {
+      if (readers[i].reader === element.reader && new Date(readers[i].start_timestamp) < elementTime &&
+        (readers[i].end_timestamp == null || elementTime < new Date(readers[i].end_timestamp)))
+        {
+          return readers[i];
+        }
+  }
+  return 0;
+}
+
+var arrayUnique = function(a) {
+    return a.reduce(function(p, c) {
+        if (p.indexOf(c) < 0) p.push(c);
+        return p;
+    }, []);
+};
+
+function drawPoints(locations)
+{
+  console.log(svg.selectAll(".reader-location").data())
+  if (locations == 0)
+  {
+    var locations = {
+      "type": "FeatureCollection",
+      "features": svg.selectAll(".reader-location").data()
+    };
+  }
   if (locations.features.length > 0)
   {
     //Zoom in to the correct area of the map
@@ -78,23 +191,21 @@ function drawReaders(readersInfo)
       .translate([0, 0]);
     var b = path.bounds(locations);
 
-    //Change bounds if there is only one points so that s does not equal infinity
-    if (b[0][0] === b[1][0] && b[0][1] === b[1][1])
-    {
-      b[0][0] *= 0.9;
-      b[0][1] *= 0.9;
+    var zoom = +slider[0][0].value;
+    var currRadius = radius * 10 / (zoom+5);
+    zoom = zoom*zoom;
 
-      b[1][0] *= 1.1;
-      b[1][1] *= 1.1;
-    }
+    b[0][0] *= 1 - (zoom/1000);
+    b[0][1] *= 1 - (zoom/1000);
+
+    b[1][0] *= 1 + (zoom/1000);
+    b[1][1] *= 1 + (zoom/1000);
 
     var s = 0.95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
     var t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
     projection
       .scale(s)
       .translate(t);
-
-    console.log(b,s,t);
 
     //Bind data
     var statesSvg = svg.selectAll(".state").data(states);
@@ -144,7 +255,6 @@ function drawReaders(readersInfo)
           tagList.exit().remove();
         });
       })
-      .attr("r", radius)
       .attr("class", "reader-location")
       .append("svg:title");
 
@@ -153,181 +263,28 @@ function drawReaders(readersInfo)
     statesSvg.attr("d", path);
     riversSvg.attr("d", path);
     lakesSvg.attr("d", path);
-    readersSvg.attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
-      .select("title")
-      .text(function (d) {return "Reader: " + d.properties.reader + "\n";});
-
-    //Exit
-    statesSvg.exit().remove();
-    riversSvg.exit().remove();
-    lakesSvg.exit().remove();
-    readersSvg.exit().remove();
-  }
-}
-
-function update(reads, readersInfo)
-{
-  var locations = {
-    "type": "FeatureCollection",
-    "features": []
-  };
-
-  var element;
-  var elementTime;
-  var index;
-
-    if (reads.results.length === 0) alert("Tag " + reads.tag + " has no reads!");
-    for (var i = 0; i < reads.results.length; i++)
-    {
-        element = reads.results[i];
-        index = -1;
-        elementTime = new Date(element.tag_timestamp);
-
-        for (var j = 0; j < locations.features.length; j++)
-        {
-          var currReaderProperties = locations.features[j].properties;
-
-          if ( currReaderProperties.name === element.reader && new Date(currReaderProperties.start_timestamp) < elementTime
-            && (currReaderProperties.end_timestamp == null || elementTime < new Date(currReaderProperties.end_timestamp)))
-          {
-            index = j;
-            break;
-          }
-        }
-
-        if (index < 0)
-        {
-          var readerCorrect = searchForReader(readersInfo.results, element);
-          reader = {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [readerCorrect.longitude, readerCorrect.latitude]
-            },
-            "properties": {
-              "name": element.reader,
-              "reads" : [],
-              "start_timestamp": readerCorrect.start_timestamp,
-              "end_timestamp": readerCorrect.end_timestamp
-            }
-          };
-          coordinates.text(reader.geometry.coordinates);
-          locations.features.push(reader);
-        }
-        else
-        {
-          reader = locations.features[index];
-        }
-
-        var read = {
-          "timestamp": element.tag_timestamp,
-          "tag_id": element.tag
-        };
-
-        reader.properties.reads.push(read);
-    }
-
-  if (locations.features.length > 0)
-  {
-    //Zoom in to the correct area of the map
-    projection
-      .scale(1)
-      .translate([0, 0]);
-    var b = path.bounds(locations);
-
-    //Change bounds if there is only one points so that s does not equal infinity
-    if (b[0][0] === b[1][0] && b[0][1] === b[1][1])
-    {
-      b[0][0] *= 0.9;
-      b[0][1] *= 0.9;
-
-      b[1][0] *= 1.1;
-      b[1][1] *= 1.1;
-    }
-
-    var s = 0.95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height);
-    var t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
-    projection
-      .scale(s)
-      .translate(t);
-
-    console.log(b,s,t);
-
-    //Bind data
-    var statesSvg = svg.selectAll(".state").data(states);
-    var riversSvg = svg.selectAll(".river").data(rivers);
-    var lakesSvg = svg.selectAll(".lake").data(lakes);
-    var readersSvg = svg.selectAll(".reader-location").data(locations.features);
-
-    //Update
-    statesSvg.classed("updated", true);
-    riversSvg.classed("updated", true);
-    lakesSvg.classed("updated", true);
-    readersSvg.classed("updated", true);
-
-    //Enter
-    statesSvg.enter()
-      .append("path")
-      .attr("class", "state")
-      .attr("id", function(d) {return d.id;})
-      .attr("fill", function(d,i) {return colorScale(((i*i)/3+7)%50);});
-
-    riversSvg.enter()
-      .append("path")
-      .attr("class", "river")
-      .attr("id", function(d) { return d.properties.name;});
-
-    lakesSvg.enter()
-      .append("path")
-      .attr("class", "lake")
-      .attr("id", function(d) { return d.properties.name;});
-
-    readersSvg.enter()
-      .append("circle")
-      .attr("r", radius)
-      .attr("class", "reader-location")
-      .append("svg:title");
-
-
-    //Enter + Update
-    statesSvg.attr("d", path);
-    riversSvg.attr("d", path);
-    lakesSvg.attr("d", path);
-    readersSvg.attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
+    readersSvg.attr("r", currRadius)
+      .attr("transform", function(d) { return "translate(" + projection(d.geometry.coordinates) + ")"; })
       .select("title")
       .text(function (d) {
-      var result = "Reader: " + d.properties.name + "\n";
-      for (var i = 0; i < d.properties.reads.length; i++)
-      {
-        result += "Tag ID: " + d.properties.reads[i].tag_id + " Time: " + new Date(d.properties.reads[i].timestamp) + "\n";
-      }
-      return result;
-    });
+        console.log(combo[0][0].value);
+        if (combo[0][0].value === "Show all readers")
+        {
+          return "Reader: " + d.properties.reader + "\n";
+        }
+        else {
+          var result = "Reader: " + d.properties.name + "\n";
+          for (var i = 0; i < d.properties.reads.length; i++)
+          {
+            result += "Tag ID: " + d.properties.reads[i].tag_id + " Time: " + new Date(d.properties.reads[i].timestamp) + "\n";
+          }
+          return result;
+        }});
 
     //Exit
     statesSvg.exit().remove();
     riversSvg.exit().remove();
     lakesSvg.exit().remove();
     readersSvg.exit().remove();
-    }
-}
-function searchForReader(readers,element)
-{
-  var elementTime = new Date(element.tag_timestamp);
-  for (var i = 0; i < readers.length; i++)
-  {
-      if (readers[i].reader === element.reader && new Date(readers[i].start_timestamp) < elementTime &&
-        (readers[i].end_timestamp == null || elementTime < new Date(readers[i].end_timestamp)))
-        {
-          return readers[i];
-        }
   }
-  return 0;
 }
-
-var arrayUnique = function(a) {
-    return a.reduce(function(p, c) {
-        if (p.indexOf(c) < 0) p.push(c);
-        return p;
-    }, []);
-};
