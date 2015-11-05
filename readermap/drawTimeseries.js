7c959792c8d41e611ad4806ea828cb09e9a8f005
nbgraham <nickbgraham83@gmail.com>
2015-11-05 13:00:02 -0600

    Switched from API calls to parsing for grouping data
    Also cleaned up code and appearance of timeseries

--- readermap/drawTimeseries.js
+++ readermap/drawTimeseries.js
@@ -1,181 +1,222 @@
 function drawReaderTimes(url, selector, outerWidth, outerHeight)
 {
+  //Configurables
   var margin = {top:30,bottom:30,left:30,right:30};
-  var axisSpace = 100;
-  var gap = 5;
   var lineWidth = 2;
-  var innerWidth = outerWidth - margin.left - margin.right;
-  var innerHeight = outerHeight - margin.top - margin.bottom;
-
-  d3.json(url, function(data) {
-  var svg = d3.select(selector).append("svg")
-      .attr("width", outerWidth)
-      .attr("height", outerHeight + 100);
-
-  var plotArea = svg.append("g")
-      .attr("transform","translate(" + margin.right + "," + margin.top + ")");
-
-  //Get data in right form
-  var results = data.results, url;
-  results.forEach(function(read) {read.tag_timestamp = new Date(read.tag_timestamp);});
 
   //Set up
+  var innerWidth = outerWidth - margin.left - margin.right;
+  var innerHeight = outerHeight - margin.top - margin.bottom;
+  var svg = d3.select(selector).append("svg")
+      .attr("width", outerWidth)
+      .attr("height", outerHeight);
+  var plotArea = svg.append("g")
+    .attr("transform","translate(" + margin.left + "," + margin.top + ")");
   var colorScale = d3.scale.category10();
   var xScale = d3.time.scale.utc()
     .range([0, innerWidth]);
 
-  colorScale.domain(d3.extent(results, function(d) {return d.tag;}));
-  xScale.domain(d3.extent(results, function(d) {return d.tag_timestamp;}));
+  d3.json(url, function(data) {
+    //Get data in right form
+    time = (new Date()).getTime();
+    tagsList = groupByTag(data.results);
+    time = (new Date()).getTime() - time;
+    console.log("Time for parsing data into groups: " + time);
+    console.log(tagsList);
 
-  var tags = plotArea.selectAll("g").data(results);
-  var rectHeight = innerHeight;
+    colorScale.domain(d3.extent(tagsList, function(d, i) {return i}));
+    var xMin = d3.min(tagsList, function(d) {return d3.min(d,function(e) {return e.tag_timestamp;});});
+    var xMax = d3.max(tagsList, function(d) {return d3.max(d,function(e) {return e.tag_timestamp;});});
+    xScale.domain([xMin,xMax]);
 
-  var xAxis = d3.svg.axis()
-      .orient("bottom")
-      .scale(xScale)
-      .ticks(7)
-      .tickFormat(d3.time.format("%Y-%m-%d %H:%M"));
-  svg.append("g")
-      .attr("class", "axis")   // give it a class so it can be used to select only xaxis labels  below
-      .attr("transform", "translate("+ margin.left + "," + outerHeight + ")")
-      .call(xAxis);
+    var xAxis = d3.svg.axis()
+        .orient("bottom")
+        .scale(xScale)
+        .ticks(10)
+        .tickFormat(d3.time.format("%m-%d %H:%M"));
+    svg.append("g")
+        .attr("class", "axis")   // give it a class so it can be used to select only xaxis labels  below
+        .attr("transform", "translate("+ margin.left + "," + (margin.top + innerHeight/2) + ")")
+        .call(xAxis);
 
-  // now rotate text on x axis
-  // solution based on idea here: https://groups.google.com/forum/?fromgroups#!topic/d3-js/heOBPQF3sAY
-  // first move the text left so no longer centered on the tick
-  // then rotate up to get 90 degrees.
- svg.selectAll(".axis text")  // select all the text elements for the xaxis
-    .attr("transform", function(d) {
-        return "translate(" + this.getBBox().height*-1 + "," + this.getBBox().height*5 + ")rotate(-90)";
+    // now rotate text on x axis
+    // solution based on idea here: https://groups.google.com/forum/?fromgroups#!topic/d3-js/heOBPQF3sAY
+    // first move the text left so no longer centered on the tick
+    // then rotate up to get 90 degrees.
+    svg.selectAll(".axis text")  // select all the text elements for the xaxis
+      .attr("transform", function(d) {
+          return "translate(" + this.getBBox().height*-1 + "," + this.getBBox().height*4 + ")rotate(-90)";
+      });
+
+    var rectHeight = innerHeight/2;
+
+    plotArea.append("rect")
+      .attr("class","background-rect")
+      .attr("width",innerWidth)
+      .attr("height", rectHeight)
+      .attr("fill", "#EAEAD5");
+    plotArea.append("text")
+        .attr("transform","translate(" + (innerWidth) + "," + rectHeight/2 + ")rotate(90)")
+        .attr("text-anchor","middle")
+        .text(tagsList[0][0].reader);
+
+    tagsList.forEach(function(tagReads) {
+    //Enter phase - add lines from reads
+    var readLines = plotArea.append("g").attr("id",tagReads[0].tag).selectAll("line").data(tagReads);
+    readLines.enter().append("line")
+      .attr("class","time-line")
+      .attr("x1", function(d) {return xScale(d.tag_timestamp);})
+      .attr("y1", 0)
+      .attr("x2", function(d) {return xScale(d.tag_timestamp);})
+      .attr("y2", rectHeight)
+      .attr("stroke", function (d) {return colorScale(d.tag);})
+      .attr("stroke-width", lineWidth)
+      .on('click', function(d) {
+        $("#combo option").each(function() {
+          if($(this).text() == d.tag) {
+            $(this).attr('selected', 'selected');
+          }
+        });
+        $("#combo").change();
+      })
+      .on('mouseover', function() {
+        d3.select(this.parentNode.parentNode).selectAll("line").attr("opacity","0.05");
+        d3.select(this.parentNode).selectAll("line").attr("opacity","1");
+      })
+      .on('mouseout', function() {
+        d3.select(this.parentNode.parentNode).selectAll("line").attr("opacity","1");
+      });
+    });
   });
-
-  var readerGroup = plotArea.append("g").attr("class","tag_group");
-  readerGroup.append("rect")
-    .attr("class","background-rect")
-    .attr("width",innerWidth)
-    .attr("height", rectHeight)
-    .attr("fill", "gray");
-  readerGroup.append("text")
-      .attr("transform","translate(" + (innerWidth+10) + "," + rectHeight/2 + ")rotate(-90)")
-      .text(results[0].reader);
-
-  //Enter phase - add lines from reads
-  var readLine = readerGroup.selectAll("line").data(results);
-  readLine.enter().append("line")
-    .attr("class","time-line")
-    .attr("x1", function(d) {return xScale(d.tag_timestamp);})
-    .attr("y1", 0)
-    .attr("x2", function(d) {return xScale(d.tag_timestamp);})
-    .attr("y2", rectHeight)
-    .attr("stroke", function (d) {return colorScale(d.tag);})
-    .attr("stroke-width",lineWidth);
-});
 }
 
 function drawTagTimes(url, selector, outerWidth, outerHeight)
 {
+  //Configurables
   var margin = {top:30,bottom:30,left:30,right:30};
-  var axisSpace = 100;
-  var gap = 5;
   var lineWidth = 2;
-  var innerWidth = outerWidth - margin.left - margin.right;
-  var innerHeight = outerHeight - margin.top - margin.bottom;
-
-  d3.json(url, function(data) {
-  var svg = d3.select(selector).append("svg")
-      .attr("width", outerWidth)
-      .attr("height", outerHeight + 100);
-
-  var plotArea = svg.append("g")
-      .attr("transform","translate(" + margin.right + "," + margin.top + ")");
-
-  //Get data in right form
-  var results = groupByTag(data.results);
+  gap = 5;
 
   //Set up
+  var innerWidth = outerWidth - margin.left - margin.right;
+  var innerHeight = outerHeight - margin.top - margin.bottom;
+  var svg = d3.select(selector).append("svg")
+      .attr("width", outerWidth)
+      .attr("height", outerHeight);
+  var plotArea = svg.append("g")
+    .attr("transform","translate(" + margin.left + "," + margin.top + ")");
   var colorScale = d3.scale.category10();
   var xScale = d3.time.scale.utc()
     .range([0, innerWidth]);
 
-  colorScale.domain(d3.extent(results, function(d) {return d.tag;}));
-  xScale.domain(d3.extent(results, function(d) {return d.tag_timestamp;}));
+  d3.json(url, function(data) {
+    //Get data in right form
+    readersList = groupByReader(data.results);
 
-  var tags = plotArea.selectAll("g").data(results);
-  var rectHeight = innerHeight;
+    colorScale.domain(d3.extent(readersList, function(d, i) {return i}));
+    var xMin = d3.min(readersList, function(d) {return d3.min(d,function(e) {return e.tag_timestamp;});});
+    var xMax = d3.max(readersList, function(d) {return d3.max(d,function(e) {return e.tag_timestamp;});});
+    xScale.domain([xMin,xMax]);
 
-  var xAxis = d3.svg.axis()
-      .orient("bottom")
-      .scale(xScale)
-      .ticks(7)
-      .tickFormat(d3.time.format("%Y-%m-%d %H:%M"));
-  svg.append("g")
-      .attr("class", "axis")   // give it a class so it can be used to select only xaxis labels  below
-      .attr("transform", "translate("+ margin.left + "," + outerHeight + ")")
-      .call(xAxis);
+    var rectHeight = innerHeight/(readersList.length + 1) - gap;
 
-  // now rotate text on x axis
-  // solution based on idea here: https://groups.google.com/forum/?fromgroups#!topic/d3-js/heOBPQF3sAY
-  // first move the text left so no longer centered on the tick
-  // then rotate up to get 90 degrees.
- svg.selectAll(".axis text")  // select all the text elements for the xaxis
-    .attr("transform", function(d) {
-        return "translate(" + this.getBBox().height*-1 + "," + this.getBBox().height*5 + ")rotate(-90)";
+    var xAxis = d3.svg.axis()
+        .orient("bottom")
+        .scale(xScale)
+        .ticks(10)
+        .tickFormat(d3.time.format("%m-%d %H:%M"));
+    svg.append("g")
+        .attr("class", "axis")   // give it a class so it can be used to select only xaxis labels  below
+        .attr("transform", "translate("+ margin.left + "," + (margin.top + innerHeight-rectHeight) + ")")
+        .call(xAxis);
+
+    // now rotate text on x axis
+    // solution based on idea here: https://groups.google.com/forum/?fromgroups#!topic/d3-js/heOBPQF3sAY
+    // first move the text left so no longer centered on the tick
+    // then rotate up to get 90 degrees.
+    svg.selectAll(".axis text")  // select all the text elements for the xaxis
+      .attr("transform", function(d) {
+          return "translate(" + this.getBBox().height*-1 + "," + this.getBBox().height*4 + ")rotate(-90)";
+      });
+
+    readersList.forEach(function(readerData, i) {
+      var readerGroup = plotArea.append("g")
+        .attr("transform","translate(0," + i*(rectHeight+gap) + ")");
+
+      readerGroup.append("rect")
+        .attr("class","background-rect")
+        .attr("width",innerWidth)
+        .attr("height", rectHeight)
+        .attr("fill", "#EAEAD5");
+      readerGroup.append("text")
+          .attr("transform","translate(" + (innerWidth) + "," + rectHeight/2 + ")rotate(90)")
+          .attr("text-anchor","middle")
+          .text(readerData[0].reader);
+
+      var readLines = readerGroup.append("g").selectAll("line").data(readerData);
+      //Enter phase - add lines from reads
+      readLines.enter().append("line")
+        .attr("class","time-line")
+        .attr("x1", function(d) {return xScale(d.tag_timestamp);})
+        .attr("y1", 0)
+        .attr("x2", function(d) {return xScale(d.tag_timestamp);})
+        .attr("y2", rectHeight)
+        .attr("stroke", function (d) {return colorScale(d.tag);})
+        .attr("stroke-width", lineWidth);
+    });
   });
-
-  var readerGroup = plotArea.selectAll("g background-rect").data(results).enter().append("g").attr("class","tag_group");
-  readerGroup.append("rect")
-    .attr("class","background-rect")
-    .attr("width", innerWidth)
-    .attr("height", rectHeight)
-    .attr("fill", "gray");
-  readerGroup.append("text")
-      .attr("transform","translate(" + (innerWidth+10) + "," + rectHeight/2 + ")rotate(-90)")
-      .text(results[0].reader);
-
-  //Enter phase - add lines from reads
-  var readLine = readerGroup.selectAll("line").data(results);
-  readLine.enter().append("line")
-    .attr("class","time-line")
-    .attr("x1", function(d) {return xScale(d.tag_timestamp);})
-    .attr("y1", 0)
-    .attr("x2", function(d) {return xScale(d.tag_timestamp);})
-    .attr("y2", rectHeight)
-    .attr("stroke", function (d) {return colorScale(d.tag);})
-    .attr("stroke-width",lineWidth);
-});
 }
 
-function groupByTag(data, url)
+function groupByTag(data)
 {
-  var uniqueTags = [];
   var results = [];
-  var element, index, tag;
-
   for (var i = 0; i<data.length; i++)
   {
-    element = data[i];
-    element.timestamp = new Date(element.timestamp);
-    index = contains(uniqueTags, element.tag);
-
+    var element = data[i];
+    element.tag_timestamp = new Date(element.tag_timestamp);
+    var index = -1;
+    results.forEach(function(tagList, i) {
+      if (tagList[0].tag == element.tag){
+        index = i;
+      }
+    });
+    var tag = [];
     if (index < 0)
     {
-      uniqueTags.push(element.tag);
-      d3.json(url + "&tag=" + element.tag, function (data)
-    {
-      results.push(data.results);
-    });
+      results.push(tag);
     }
-
+    else
+    {
+      tag = results[index];
+    }
+    tag.push(element);
   }
   return results;
 }
 
-function contains(list, value)
+function groupByReader(data)
 {
-  for (var i = 0; i < list.length; i++)
+  var results = [];
+  for (var i = 0; i<data.length; i++)
   {
-    if (list[i] === value)
-      return i;
+    var element = data[i];
+    element.tag_timestamp = new Date(element.tag_timestamp);
+    var index = -1;
+    results.forEach(function(tagList, i) {
+      if (tagList[0].reader == element.reader){
+        index = i;
+      }
+    });
+    var tag = [];
+    if (index < 0)
+    {
+      results.push(tag);
+    }
+    else
+    {
+      tag = results[index];
+    }
+    tag.push(element);
   }
-return -1;
+  return results;
 }
